09ee8defc5774ba2fff9978f8dc338192d297eef
pravin nandankar <pravinnandankar03@gmail.com>
2024-01-02 20:20:09 +0530

    envv

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/portfolio",)
+require('dotenv').config()
+mongoose.connect(process.env.DB)
+// mongoose.connect("mongodb://localhost:27017/portfolio",)
   .then(() => {
     console.log('Connected to MongoDB');
   })
